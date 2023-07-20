3747eebc29623ea01129ad43fcb67cd170f49b9e
Zhaoyuan Gu <guzhaoyuan14@gmail.com>
2023-07-20 13:52:00 -0400

    Implement PD controller in callback. Use userdata as target position.

--- mj_cassie.py
+++ mj_cassie.py
@@ -81,6 +81,9 @@ class CassieEnv(MujocoEnv, utils.EzPickle):
             'reference_trajectories/cassie_walk/old_cassie_reference_qpos_list.npy')
         self.ref_qvel = np.load(
             'reference_trajectories/cassie_walk/old_cassie_reference_qvel_list.npy')
+        # Index from README.
+        self.p_index = [7, 8, 9, 14, 20, 21, 22, 23, 28, 34]
+        self.v_index = [6, 7, 8, 12, 18, 19, 20, 21, 25, 31]
         # print(self.ref_qpos.shape)
 
         self.timestamp = 0
@@ -143,23 +146,32 @@ class CassieEnv(MujocoEnv, utils.EzPickle):
         # PD gain from old cassie env.
         kp = np.array([100, 100, 88, 96, 50, 100, 100, 88, 96, 50])
         kd = np.array([10.0, 10.0, 8.0, 9.6, 5.0, 10.0, 10.0, 8.0, 9.6, 5.0])
-        # Index from README.
-        p_index = [7, 8, 9, 14, 20, 21, 22, 23, 28, 34]
-        v_index = [6, 7, 8, 12, 18, 19, 20, 21, 25, 31]
-        p = self.data.qpos[p_index]
-        v = self.data.qvel[v_index]
+        p = self.data.qpos[self.p_index]
+        v = self.data.qvel[self.v_index]
         v_desired = np.zeros(10)
         return kp * (p_desired - p) + kd * (v_desired - v)
+    
+    def _step_mujoco_simulation(self, ctrl, n_frames):
+        # Set the control target, this userdata is used by PD control callback.
+        self.data.userdata[:] = ctrl
 
+        mujoco.mj_step(self.model, self.data, nstep=n_frames)
+
+        # As of MuJoCo 2.0, force-related quantities like cacc are not computed
+        # unless there's a force sensor in the model.
+        # See https://github.com/openai/gym/issues/1541
+        mujoco.mj_rnePostConstraint(self.model, self.data)
+        
     # The action is now the target position.
     def step(self, action):
         self.timestamp += 1
         ref_qpos, ref_qvel = self.ref_qpos[self.timestamp], self.ref_qvel[self.timestamp]
         xy_position_before = mass_center(self.model, self.data)
 
+        q_pos_modified = action + ref_qpos[self.p_index]
         # Simulate at 2000 Hz for frame_skip times.
         torque = self.PD(action)
-        self.do_simulation(torque, self.frame_skip)
+        self._step_mujoco_simulation(q_pos_modified, self.frame_skip)
 
         xy_position_after = mass_center(self.model, self.data)
         # Transition happens here so time + 1
@@ -252,6 +264,7 @@ class CassieEnv(MujocoEnv, utils.EzPickle):
 
         return observation, reward, terminated, False, info
 
+    
     def reset_model(self):
         noise_low = -self._reset_noise_scale
         noise_high = self._reset_noise_scale
@@ -266,6 +279,22 @@ class CassieEnv(MujocoEnv, utils.EzPickle):
 
         observation = self._get_obs()
         self.timestamp = 0
+        
+        self.data.userdata = np.zeros(10) # Use userdata as target position.
+        # Define a callback that modify the ctrl before mj_step.
+        def PD_control_CB(model, data):
+            kp = np.array([100, 100, 88, 96, 50, 100, 100, 88, 96, 50])
+            kd = np.array([10.0, 10.0, 8.0, 9.6, 5.0, 10.0, 10.0, 8.0, 9.6, 5.0])
+            p_index = [7, 8, 9, 14, 20, 21, 22, 23, 28, 34]
+            v_index = [6, 7, 8, 12, 18, 19, 20, 21, 25, 31]
+            p = data.qpos[p_index]
+            v = data.qvel[v_index]
+            p_desired = data.userdata[:]
+            v_desired = np.zeros(10)
+            data.ctrl[:] = kp * (p_desired - p) + kd * (v_desired - v)
+            
+        mujoco.set_mjcb_control(None)
+        mujoco.set_mjcb_control(lambda m, d: PD_control_CB(m, d))
 
         return observation
 
