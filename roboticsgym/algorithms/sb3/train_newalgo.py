2d0e0faadb2defea97935b96a851521bdc535364
Feiyang Wu <fwu91@gatech.edu>
2023-10-17 14:54:28 -0400

    Clean up repo files

---
+++ roboticsgym/algorithms/sb3/train_newalgo.py
@@ -0,0 +1,309 @@
+import gymnasium as gym
+import mujoco
+import sys, os
+from typing import Callable
+import datetime
+import time
+import optuna
+from stable_baselines3.common import type_aliases
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback, BaseCallback, CallbackList, EventCallback
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder, VecEnv, sync_envs_normalization, is_vecenv_wrapped
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.sac import SAC
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common import base_class  # pytype: disable=pyi-error
+from newAlgo import HIP, EvalStudentCallback, evaluate_student_policy
+from old_cassie.cassie_env.cassieRLEnvMirror import cassieRLEnvMirror
+from oldcassie import OldCassieMirrorEnv
+from inversepolicies import IPMDPolicy
+import wandb
+from wandb.integration.sb3 import WandbCallback
+from gymnasium.envs.registration import register
+import warnings
+import numpy as np
+
+
+try:
+    from tqdm import TqdmExperimentalWarning
+
+    # Remove experimental warning
+    warnings.filterwarnings("ignore", category=TqdmExperimentalWarning)
+    from tqdm.rich import tqdm
+except ImportError:
+    # Rich not installed, we only throw an error
+    # if the progress bar is used
+    tqdm = None
+
+register(
+        id='CassieMirror-v1',
+        entry_point='arm_cassie_env.cassie_env.oldCassie:OldCassieMirrorEnv',
+        max_episode_steps=600,
+        )
+
+
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+
+    return func
+
+def train(env_id: str = "CassieMirror-v1"):
+    config = {
+        "policy_type": "MlpPolicy",
+        "total_timesteps": 1e7,
+        "env_id": env_id,
+        'buffer_size': 1000000,
+        'train_freq': 3,
+        'gradient_steps': 3,
+        "progress_bar": True,
+        "verbose": 0,
+        'ent_coef': 0.01,
+        'student_ent_coef': 0.01,
+        'learning_rate': linear_schedule(5e-3),
+        "n_envs": 24,
+        'batch_size': 300,
+        'seed': 42,
+        'expert_replaybuffersize': 600,
+        'expert_replaybuffer': f'expert_demo/SAC/10traj_morestable',
+        'student_begin': int(0),
+        'teacher_gamma': 1.00,
+        'student_gamma': 1.00,
+        'reward_reg_param': 0.05
+    } 
+    run = wandb.init(
+        project="IRL HIP Tuning",
+        config=config,
+        name=f'{env_id}-{time.strftime("%Y-%m-%d-%H-%M-%S")}',
+        tags=[f'{env_id}', 'Old Cassie'],
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+        # monitor_gym=True,  # auto-upload the videos of agents playing the game
+        save_code=True,  # optional
+        reinit=True,
+        notes="student coef follows teacher and use off policy evaluation"
+    )
+    wandb.run.log_code(".")
+
+    wandbcallback = WandbCallback(
+            # gradient_save_freq=5000,
+        )
+    # Create log dir
+    train_env = make_vec_env(config['env_id'], n_envs=config['n_envs'],vec_env_cls=SubprocVecEnv)
+    # Separate evaluation env
+    eval_env = make_vec_env(config['env_id'], n_envs=1,vec_env_cls=SubprocVecEnv)
+    # Use deterministic actions for evaluation
+    eval_callback = EvalCallback(eval_env, 
+                                best_model_save_path=f"logs/{run.project}/{run.name}/teacher/",
+                                log_path=f"logs/{run.project}/{run.name}/teacher/", 
+                                eval_freq=2000,
+                                n_eval_episodes=3,
+                                deterministic=True, 
+                                render=False,
+                                verbose=1)
+    student_eval_env = make_vec_env(config['env_id'], n_envs=1,vec_env_cls=SubprocVecEnv)
+    eval_student_callback = EvalStudentCallback(student_eval_env,
+                                                best_model_save_path=f"logs/{run.project}/{run.name}/student/",
+                                                log_path=f"logs/{run.project}/{run.name}/student/", 
+                                                eval_freq=1000,
+                                                n_eval_episodes=3,
+                                                deterministic=True, 
+                                                render=False,
+                                                verbose=1)
+    callback_list = CallbackList([eval_callback, wandbcallback, eval_student_callback])
+    # Init model
+    irl_model = HIP(policy='IPMDPolicy', 
+                    student_policy='IPMDPolicy',
+                    env=train_env, 
+                    gamma=config['teacher_gamma'],
+                    verbose=config['verbose'],
+                    student_gamma=config['student_gamma'],
+                    buffer_size=config['buffer_size'],
+                    ent_coef=config['ent_coef'], 
+                    student_ent_coef=config['student_ent_coef'],
+                    batch_size=config['batch_size'], 
+                    learning_rate=config['learning_rate'],
+                    gradient_steps=config['gradient_steps'],
+                    expert_replaybuffer=config['expert_replaybuffer'], 
+                    expert_replaybuffersize=config['expert_replaybuffersize'],
+                    tensorboard_log=f'logs/tensorboard/{run.name}/',
+                    seed=config['seed'],
+                    learning_starts=100,
+                    student_begin=config['student_begin'],
+                    reward_reg_param=config['reward_reg_param'],
+                    )
+    # irl_model.set_parameters('/home/feiyang/Develop/Cassie/arm-cassie/logs/2023-07-05-21-23-30/student/best_model.zip')
+    # Model learning
+    irl_model.learn(
+        total_timesteps=config['total_timesteps'], 
+        callback=callback_list,
+        progress_bar=config['progress_bar'],
+        log_interval=100,
+        )
+    # Evaluation
+    _, _ = evaluate_policy(irl_model, eval_env, n_eval_episodes=10)
+    _, _ = evaluate_student_policy(irl_model, eval_env, n_eval_episodes=10)
+    
+    # Finish wandb run
+    run.finish()
+
+
+def visualize_best_student():
+    config = {
+        "policy_type": "MlpPolicy",
+        "total_timesteps": 5e6,
+        "env_id": "CassieMirror-v1",
+        'buffer_size': 1000000,
+        'train_freq': 3,
+        'gradient_steps': 3,
+        "progress_bar": True,
+        "verbose": 1,
+        'ent_coef': 'auto',
+        'student_ent_coef': 0.01,
+        'learning_rate': 5e-3,
+        "n_envs": 1,
+        'batch_size': 512,
+        'seed': 1,
+        'expert_replaybuffersize': 600,
+        'expert_replaybuffer': 'expert_demo/SAC/10traj_morestable',
+    }
+    
+    # Create log dir
+    train_env = make_vec_env(config['env_id'], n_envs=config['n_envs'],vec_env_cls=SubprocVecEnv)
+	# Separate evaluation env
+    eval_env = make_vec_env(config['env_id'], 
+                            n_envs=1,
+                            vec_env_cls=SubprocVecEnv,
+                            env_kwargs={'render': True})
+	
+	# Init model
+    irl_model = HIP.load('logs/2023-07-07-11-16-17/student/best_model.zip')
+    irl_model.set_parameters('logs/2023-07-08-21-42-09/student/best_model.zip')
+    
+    # Evaluation
+    mean_student_reward, _ = evaluate_student_policy(irl_model, eval_env, n_eval_episodes=10)
+    
+    # Finish wandb run
+
+    return mean_student_reward
+
+def run_mujoco_experiments():
+    for env_id in ['Hopper-v4', 'Humanoid-v4', 'Walker2d-v4']:
+        config = {
+        "policy_type": "IPMDPolicy",
+        "total_timesteps": 5e6,
+        "env_id": env_id,
+        'buffer_size': 1000000,
+        'train_freq': 3,
+        'gradient_steps': 3,
+        "progress_bar": True,
+        "verbose": 1,
+        'ent_coef': '0.001',
+        'student_ent_coef': 0.001,
+        'learning_rate': 3e-4,
+        "n_envs": 12,
+        'batch_size': 512,
+        'seed': 42,
+        'expert_replaybuffersize': 1000,
+        'expert_replaybuffer': f'logs/{env_id}/expert_rb',
+        'student_begin': int(0),
+        'teacher_gamma': 1.00,
+        'student_gamma': 1.00,
+        'reward_reg_param': 0.05
+        } 
+        run = wandb.init(
+            project="HIP MuJoCo",
+            config=config,
+            name=f'{env_id}-{time.strftime("%Y-%m-%d-%H-%M-%S")}',
+            tags=[f'{env_id}', 'MuJoCo Examples'],
+            sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+            monitor_gym=True,  # auto-upload the videos of agents playing the game
+            save_code=True,  # optional
+            reinit=True,
+            notes="student has fixed ent coef, teacher has auto ent coef"
+        )
+        wandb.run.log_code(".")
+
+        wandbcallback = WandbCallback(
+                gradient_save_freq=5000,
+            )
+        # Create log dir
+        train_env = make_vec_env(config['env_id'], n_envs=config['n_envs'],vec_env_cls=SubprocVecEnv)
+        # Separate evaluation env
+        eval_env = make_vec_env(config['env_id'], n_envs=1,vec_env_cls=SubprocVecEnv)
+        # Use deterministic actions for evaluation
+        eval_callback = EvalCallback(eval_env, 
+                                    best_model_save_path=f"logs/{run.project}/{run.name}/teacher/",
+                                    log_path=f"logs/{run.project}/{run.name}/teacher/", 
+                                    eval_freq=2000,
+                                    n_eval_episodes=3,
+                                    deterministic=True, 
+                                    render=False,
+                                    verbose=1)
+        student_eval_env = make_vec_env(config['env_id'], n_envs=1,vec_env_cls=SubprocVecEnv)
+        eval_student_callback = EvalStudentCallback(student_eval_env,
+                                                    best_model_save_path=f"logs/{run.project}/{run.name}/student/",
+                                                    log_path=f"logs/{run.project}/{run.name}/student/", 
+                                                    eval_freq=1000,
+                                                    n_eval_episodes=3,
+                                                    deterministic=True, 
+                                                    render=False,
+                                                    verbose=1)
+        callback_list = CallbackList([eval_callback, wandbcallback, eval_student_callback])
+        # Init model
+        irl_model = HIP(policy=config['policy_type'], 
+                        student_policy='IPMDPolicy',
+                        env=train_env, 
+                        gamma=config['teacher_gamma'],
+                        verbose=config['verbose'],
+                        student_gamma=config['student_gamma'],
+                        buffer_size=config['buffer_size'],
+                        ent_coef=config['ent_coef'], 
+                        student_ent_coef=config['student_ent_coef'],
+                        batch_size=config['batch_size'], 
+                        learning_rate=config['learning_rate'],
+                        gradient_steps=config['gradient_steps'],
+                        expert_replaybuffer=config['expert_replaybuffer'], 
+                        expert_replaybuffersize=config['expert_replaybuffersize'],
+                        tensorboard_log=f'logs/tensorboard/{run.name}/',
+                        seed=config['seed'],
+                        learning_starts=100,
+                        student_begin=config['student_begin'],
+                        reward_reg_param=config['reward_reg_param'],
+                        )
+        # irl_model.set_parameters('/home/feiyang/Develop/Cassie/arm-cassie/logs/2023-07-05-21-23-30/student/best_model.zip')
+        # Model learning
+        irl_model.learn(
+            total_timesteps=config['total_timesteps'], 
+            callback=callback_list,
+            progress_bar=config['progress_bar'],
+            log_interval=100,
+            )
+        # Evaluation
+        _, _ = evaluate_policy(irl_model, eval_env, n_eval_episodes=10)
+        _, _ = evaluate_student_policy(irl_model, eval_env, n_eval_episodes=10)
+        
+        # Finish wandb run
+        run.finish()
+
+if __name__ == '__main__':
+    mean_reward, student_reward = train()
+    # print('finished! mean_reward: ', mean_reward, student_reward)
+    # visualize_best_student()
+    # array = np.load('logs/2023-07-07-11-16-17/student/evaluations.npz')
+    # print(array['results'].mean(axis=1).max())
+    # run_mujoco_experiments()
---
+++ roboticsgym/envs/digit.py
@@ -0,0 +1,240 @@
+import os
+import numpy as np
+from gymnasium.envs.mujoco import MujocoEnv
+from gymnasium import utils
+from gymnasium.spaces import Box
+import mujoco
+from mujoco._functions import mj_rnePostConstraint
+from mujoco._functions import mj_step
+
+from reference_trajectories.loadDigit import DigitTrajectory
+
+DEFAULT_CAMERA_CONFIG = {
+    "trackbodyid": 1,
+    "distance": 4.0,
+    "lookat": np.array((0.0, 0.0, 2.0)),
+    "elevation": -20.0,
+}
+
+
+def mass_center(model, data):
+    mass = np.expand_dims(model.body_mass, axis=1)
+    xpos = data.xipos
+    return (np.sum(mass * xpos, axis=0) / np.sum(mass))[0:2].copy()
+
+
+class DigitEnv(MujocoEnv, utils.EzPickle):
+    metadata = {
+        "render_modes": [
+            "human",
+            "rgb_array",
+            "depth_array",
+        ],
+        "render_fps": 400,
+    }
+
+    def __init__(
+            self,
+            forward_reward_weight=0.1,
+            ctrl_cost_weight=0.1,
+            healthy_reward=0.1,
+            terminate_when_unhealthy=True,
+            healthy_z_range=(0.5, 1.2),
+            reset_noise_scale=1e-3,
+            exclude_current_positions_from_observation=False,
+            **kwargs,
+    ):
+        self._forward_reward_weight = forward_reward_weight
+        self._ctrl_cost_weight = ctrl_cost_weight
+        self._healthy_reward = healthy_reward
+        self._terminate_when_unhealthy = terminate_when_unhealthy
+        self._healthy_z_range = healthy_z_range
+        self.timestamp = 0
+
+
+        self._reset_noise_scale = reset_noise_scale
+
+        self._exclude_current_positions_from_observation = (
+            exclude_current_positions_from_observation
+        )
+
+        if exclude_current_positions_from_observation:
+            observation_space = Box(
+                low=-np.inf, high=np.inf, shape=(78, ), dtype=np.float64
+            )
+        else:
+            observation_space = Box(
+                low=-np.inf, high=np.inf, shape=(80, ), dtype=np.float64
+            )
+
+        MujocoEnv.__init__(
+            self,
+            os.getcwd()+"/digit-v3.xml",
+            5,
+            observation_space=observation_space,
+            default_camera_config=DEFAULT_CAMERA_CONFIG,
+            **kwargs,
+        )
+        # overriding action space
+        self.action_space = Box(low=-1, high=1, shape=(20,), dtype=np.float32)
+        # print(self.action_space.shape)
+        self.ref_trajectory = DigitTrajectory("reference_trajectories/digit_state_downsample.csv")
+        
+        initial_qpos, initial_qvel = self.ref_trajectory.state(0)
+
+        self.init_qpos = initial_qpos
+        self.init_qvel = initial_qvel
+        self.ref_qpos = initial_qpos
+        self.ref_qvel = initial_qvel
+
+        # Index from README. The toes are actuated by motor A and B.
+        self.p_index = [7, 8, 9, 14, 18, 23, 30, 31, 32, 33, 
+                        34, 35, 36, 41, 45, 50, 57, 58, 59, 60]
+        self.v_index = [6, 7, 8, 12, 16, 20, 26, 27, 28, 29, 
+                        30, 31, 32, 36, 40, 44, 50, 51, 52, 53]
+        
+        self.reset_model()
+
+    @property
+    def healthy_reward(self):
+        return (
+                float(self.is_healthy or self._terminate_when_unhealthy)
+                * self._healthy_reward
+        )
+
+    def control_cost(self, action):
+        control_cost = self._ctrl_cost_weight * np.sum(np.square(self.data.ctrl))
+        return control_cost
+
+    @property
+    def is_healthy(self):
+        min_z, max_z = self._healthy_z_range
+        is_healthy = min_z < self.data.qpos[2] < max_z
+        is_healthy = is_healthy and (not np.allclose(self.data.cfrc_ext.flat.copy(), 0))
+        return is_healthy
+
+    @property
+    def terminated(self):
+        terminated = (not self.is_healthy) if self._terminate_when_unhealthy else False
+        return terminated
+
+    def _get_obs(self):
+        position = self.data.qpos.flat.copy()[self.p_index]
+        velocity = self.data.qvel.flat.copy()[self.v_index]
+
+        ref_qpos, ref_qvel = self.ref_trajectory.state(self.timestamp+1)
+
+        com_inertia = self.data.cinert.flat.copy()
+        com_velocity = self.data.cvel.flat.copy()
+
+        actuator_forces = self.data.qfrc_actuator.flat.copy()
+        external_contact_forces = self.data.cfrc_ext.flat.copy()
+
+        if self._exclude_current_positions_from_observation:
+            position = position[2:]
+
+        return np.concatenate(
+            (
+                position,
+                velocity,
+                ref_qpos[self.p_index],
+                ref_qvel[self.v_index],
+            )
+        )
+
+    def _step_mujoco_simulation(self, ctrl, n_frames):
+        # Set the control target, this userdata is used by PD control callback.
+        self.data.userdata[:] = ctrl
+
+        mj_step(self.model, self.data, nstep=n_frames)
+
+        # As of MuJoCo 2.0, force-related quantities like cacc are not computed
+        # unless there's a force sensor in the model.
+        # See https://github.com/openai/gym/issues/1541
+        mj_rnePostConstraint(self.model, self.data)
+        
+    def step(self, action):
+        ref_qpos, ref_qvel = self.ref_trajectory.state(self.timestamp+1)
+        self.ref_qpos = ref_qpos
+        self.ref_qvel = ref_qvel
+        self.timestamp += 1
+        
+        xy_position_before = mass_center(self.model, self.data)
+        # print(action)
+        q_pos_modified = action + ref_qpos[self.p_index]
+        self.frame_skip = 5
+        self._step_mujoco_simulation(q_pos_modified, self.frame_skip)
+        
+        # rod_index = [10,11,12,13, 19,20,21,22, 24,25,26,27, 
+        #              37,38,39,40, 46,47,48,49, 51,52,53,54]
+        
+        xy_position_after = mass_center(self.model, self.data)
+
+        xy_velocity = (xy_position_after - xy_position_before) / self.dt
+        x_velocity, y_velocity = xy_velocity
+
+        ctrl_cost = 0.1 * np.linalg.norm(action, ord=2)
+
+        forward_reward = self._forward_reward_weight * x_velocity
+        healthy_reward = self.healthy_reward
+        tracking_reward = \
+            0.5 * np.exp(-np.linalg.norm(ref_qpos[self.p_index] - self.data.qpos[self.p_index], ord=2)) + \
+            0.3 * np.exp(-np.linalg.norm(ref_qvel[self.v_index] - self.data.qvel[self.v_index], ord=2)) + \
+            0.2 * np.exp(-np.linalg.norm(ref_qpos[:3] - self.data.qpos[:3], ord=2))
+
+        reward = forward_reward + healthy_reward + tracking_reward - ctrl_cost
+        observation = self._get_obs()
+        terminated = self.terminated
+        info = {
+            "reward_linvel": forward_reward,
+            "reward_quadctrl": -ctrl_cost,
+            "reward_alive": healthy_reward,
+            "x_position": xy_position_after[0],
+            "y_position": xy_position_after[1],
+            "distance_from_origin": np.linalg.norm(xy_position_after, ord=2),
+            "x_velocity": x_velocity,
+            "y_velocity": y_velocity,
+            "forward_reward": forward_reward,
+        }
+
+        if self.render_mode == "human":
+            self.render()
+        return observation, reward, terminated, False, info
+
+    def reset_model(self):
+        noise_low = -self._reset_noise_scale
+        noise_high = self._reset_noise_scale
+
+        qpos = self.init_qpos 
+        qvel = self.init_qvel 
+        self.set_state(qpos, qvel)
+
+        self.timestamp = 0
+        observation = self._get_obs()
+        
+        self.data.userdata = np.zeros(20)  # Use userdata as target position.
+        # Define a callback that modify the ctrl before mj_step.
+
+        mujoco.set_mjcb_control(None)
+        mujoco.set_mjcb_control(lambda m, d: PD_control_CB(m, d))
+        
+        return observation
+
+    def viewer_setup(self):
+        assert self.viewer is not None
+        for key, value in DEFAULT_CAMERA_CONFIG.items():
+            if isinstance(value, np.ndarray):
+                getattr(self.viewer.cam, key)[:] = value
+            else:
+                setattr(self.viewer.cam, key, value)
+
+def PD_control_CB(model, data):
+    kp = np.array([100, 100, 88, 96, 50, 50, 50, 50, 50, 50, 100, 100, 88, 96, 50, 50, 50, 50, 50, 50])
+    kd = np.array([10.0, 10.0, 8.0, 9.6, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 10.0, 10.0, 8.0, 9.6, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0])
+    p_index = [7, 8, 9, 14, 18, 23, 30, 31, 32, 33, 34, 35, 36, 41, 45, 50, 57, 58, 59, 60]
+    v_index = [6, 7, 8, 12, 16, 20, 26, 27, 28, 29, 30, 31, 32, 36, 40, 44, 50, 51, 52, 53]
+    p = data.qpos[p_index]
+    v = data.qvel[v_index]
+    p_desired = data.userdata[:]
+    v_desired = np.zeros(20)
+    data.ctrl[:] = kp * (p_desired - p) + kd * (v_desired - v)
